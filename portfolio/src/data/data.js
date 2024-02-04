91e4c2397f188e4452490e09ad5850147631dd7c
shen lee <cipher>
2024-02-04 22:00:27 +0300

    my portfolio-react-website

---
+++ portfolio/src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/g25B3Qnx/aqua.jpg',
+    title: "Education",
+    desc: "",
+    link: "https://shenleeosialai.github.io/world/aqua/",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/PfgzHDP8/img.jpg',
+    title: "Restaurant website",
+    link: "https://shenleeosialai.github.io/jkmfries/jkmfries/",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/76G4gd1h/head.jpg',
+    title: "landing page",
+    desc: "",
+    link: "https://shenleeosialai.github.io/headphone-slider",
+    git: "",
+  }
+  
+];
